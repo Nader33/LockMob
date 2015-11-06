4b8917368e69535c97943b663beed3445f8301a3
Nader <nado.gharib@gmail.com>
2015-11-06 14:00:28 +0100

    design page home

--- www/js/router.js
+++ www/js/router.js
@@ -63,7 +63,7 @@ angular.module('starter.router', [])
                     controller: 'HomeCtrl'
                 },
                 'fabContent': {
-                    template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                    template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900" ><i class="icon ion-plus"></i></button>',
                     controller: function ($timeout) {
                         /*$timeout(function () {
                          document.getElementById('fab-profile').classList.toggle('on');
